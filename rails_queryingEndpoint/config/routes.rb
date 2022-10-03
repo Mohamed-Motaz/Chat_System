89d8b07dcb64e1b88fd0310f1e19316773a0cb01
Mohamed Motaz <mohamedmotaz247@yahoo.com>
2022-10-03 17:09:16 +0200

    fix the routes in rails and use mysql2 instead of sqlite

--- rails_queryingEndpoint/config/routes.rb
+++ rails_queryingEndpoint/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  namespace :api do
+    namespace :v1 do
+      get 'applications' => 'applications#index'
+      get 'applications/:token' => 'applications#show'
+      post 'applications' => 'applications#create'
+      put 'applications/:token' => 'applications#update'
+
+     
+    end
+  end
 end
